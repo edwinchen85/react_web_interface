d6ab9fc523afe933df8a4219f069010e697a7c8b
edwinchen85 <edwinchen85@gmail.com>
2017-07-11 14:24:17 +0800

    Add a new state - title and render it as heading

--- process/js/app.js
+++ process/js/app.js
@@ -2,10 +2,16 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var MainInterface = React.createClass({
+  getInitialState: function() {
+    return {
+      title: 'Appointments'
+    }
+  },
+
   render: function() {
     return (
       <div className="interface">
-        <h1>Pet Appointments</h1>
+        <h1>{this.state.title}</h1>
         <ul>
           <li>Buffy 3:30 PM</li>
           <li>Spot 8:30 PM</li>
