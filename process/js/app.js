0eb72235af0680de8d58984ef004cd992402d84a
edwinchen85 <edwinchen85@gmail.com>
2017-07-11 14:33:16 +0800

    Remove code and add data

--- process/js/app.js
+++ process/js/app.js
@@ -4,30 +4,40 @@ var ReactDOM = require('react-dom');
 var MainInterface = React.createClass({
   getInitialState: function() {
     return {
-      title: 'Appointments',
-      show: true
+      data: [
+        {
+          "petName": "Buffy",
+          "ownerName": "Hassum Harrod",
+          "aptDate": "2016-06-20 15:30",
+          "aptNotes": "This Chihuahua has not eaten for three days and is lethargic"
+        },
+        {
+          "petName": "Spot",
+          "ownerName": "Constance Smith",
+          "aptDate": "2016-06-24 08:30",
+          "aptNotes": "This German Shepherd is having some back pain"
+        },
+        {
+          "petName": "Goldie",
+          "ownerName": "Barot Bellingham",
+          "aptDate": "2016-06-22 15:50",
+          "aptNotes": "This Goldfish has some weird spots in the belly"
+        },
+        {
+          "petName": "Mitten",
+          "ownerName": "Hillary Goldwyn",
+          "aptDate": "2016-06-21 9:15",
+          "aptNotes": "Cat has excessive hairballs"
+        }
+      ]
     }
   },
 
   render: function() {
-    var showTitle;
-    if (this.state.show) {
-      showTitle = 'New';
-    }
-
-    var displayList = {
-      display: this.state.show ? 'block' : 'none',
-      color: 'red'
-    }
 
     return (
       <div className="interface">
-        <h1>{showTitle} {this.state.title}</h1>
-        <ul style={displayList}>
-          <li>Buffy 3:30 PM</li>
-          <li>Spot 8:30 PM</li>
-          <li>Goldie 3:50 PM</li>
-        </ul>
+
       </div>
     )
   }
