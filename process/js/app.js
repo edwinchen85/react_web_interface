25b51120d181d3c764829aaefcfdbc76fd985c87
edwinchen85 <edwinchen85@gmail.com>
2017-07-11 16:07:46 +0800

    Create <AptList /> and require it inside app

--- process/js/app.js
+++ process/js/app.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var AptList = require('./AptList');
 
 var MainInterface = React.createClass({
   getInitialState: function() {
@@ -39,23 +40,6 @@ var MainInterface = React.createClass({
           </ul>
         </div>
       </div>
-    )
-  }
-});
-
-var AptList = React.createClass({
-  render: function() {
-    return (
-      <li className="pet-item media">
-        <div className="pet-info-media-body">
-          <div className="pet-head">
-            <span className="pet-name">{this.props.singleItem.petName}</span>
-            <span className="apt-date pull-right">{this.props.singleItem.aptDate}</span>
-          </div>
-          <div className="owner-name"><span className="label-item">Owner:</span>{this.props.singleItem.ownerName}</div>
-          <div className="apt-notes">{this.props.singleItem.aptNotes}</div>
-        </div>
-      </li>
     )
   }
 });
