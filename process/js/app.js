1b2e6225962d382c8becc47df9051c23c6edff57
edwinchen85 <edwinchen85@gmail.com>
2017-07-11 14:30:22 +0800

    Add styles to the list

--- process/js/app.js
+++ process/js/app.js
@@ -15,10 +15,15 @@ var MainInterface = React.createClass({
       showTitle = 'New';
     }
 
+    var displayList = {
+      display: this.state.show ? 'block' : 'none',
+      color: 'red'
+    }
+
     return (
       <div className="interface">
         <h1>{showTitle} {this.state.title}</h1>
-        <ul>
+        <ul style={displayList}>
           <li>Buffy 3:30 PM</li>
           <li>Spot 8:30 PM</li>
           <li>Goldie 3:50 PM</li>
